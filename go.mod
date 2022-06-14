77ac76740a6f0239da84c52b29c9567f54e9e394
Neko2h <neko2h@gmail.com>
2022-06-14 12:19:31 +0300

    http tests, codecove,git actions

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/itchyny/base58-go v0.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.7.2 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/lib/pq v1.10.0 // indirect
---
+++ internal/app/app.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Neko2h/shortener/internal/controller"
+	"github.com/Neko2h/shortener/internal/store/cache/redis"
+	"github.com/Neko2h/shortener/internal/store/db/postgres"
+	"github.com/Neko2h/shortener/internal/usecase"
+	"github.com/labstack/echo/v4"
+)
+
+func NewApp() {
+
+	store, err := postgres.NewPgDb(os.Getenv("PG_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	cacheImp, err := redis.NewRedisClient(os.Getenv("REDIS_HOST"))
+	if err != nil {
+		panic(err)
+	}
+
+	LinkUsecase := usecase.NewLinkUsecase(postgres.NewLinkRepository(store.DB), redis.NewLinkCache(cacheImp.Client))
+
+	//postgres.NewLinkRepository(store.DB)
+	LinkController := controller.NewLinkController(LinkUsecase)
+
+	e := echo.New()
+	e.GET("/:hash", LinkController.GetLink)
+	e.POST("/", LinkController.Create)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))))
+
+}
